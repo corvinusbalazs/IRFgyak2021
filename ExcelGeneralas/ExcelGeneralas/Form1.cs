5a83e5877705fa5bbb1a2af4c0d634ae081f6385
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-08 10:47:38 +0200

    Create excel

--- ExcelGeneralas/ExcelGeneralas/Form1.cs
+++ ExcelGeneralas/ExcelGeneralas/Form1.cs
@@ -3,19 +3,26 @@ using System.Linq;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System;
+
 namespace ExcelGeneralas
+
 {
     public partial class Form1 : Form
     {
         
         RealEstateEntities context = new RealEstateEntities();
-
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
 
             
         }
@@ -23,6 +30,33 @@ namespace ExcelGeneralas
         {
             
             lakasok = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlApp = null;
+            }
+            
+        }
+        private void CreateTable()
+        { 
+        
         }
     }
 }
