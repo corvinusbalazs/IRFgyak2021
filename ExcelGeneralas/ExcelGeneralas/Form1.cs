beb4c0cc596a69bef277392b765e55f528a9dd55
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-08 10:24:34 +0200

    adatkötés

--- ExcelGeneralas/ExcelGeneralas/Form1.cs
+++ ExcelGeneralas/ExcelGeneralas/Form1.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ExcelGeneralas
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+
+        List<Flat> lakasok;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            
+        }
+        public void LoadData()
+        {
+            dataGridView1.DataSource = lakasok;
+            lakasok = context.Flats.ToList();
         }
     }
 }
