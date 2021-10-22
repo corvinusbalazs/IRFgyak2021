9f64b386351362c69a4a1891aa584701b6252c2d
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-22 19:11:43 +0200

    add db

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> nyereségekRendezve;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void CreatePortfolio()
+        {
+
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
         }
     }
 }
