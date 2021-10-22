19dd3e7d6035540f3b95cef73381c3afe033c335
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-22 13:10:32 +0200

    Create country class

---
+++ ZHgyakorlas/ZHgyakorlas/Folder/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHgyakorlas.Folder
+{
+    class Country
+    {
+        public string Nem { get; set; }
+        public string AverageRating { get; set; }
+        public string Percent { get; set; }
+    }
+}
