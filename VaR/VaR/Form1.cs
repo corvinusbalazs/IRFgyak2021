c89230ee4453057260184eb3ed9f8cccf0e725d0
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-22 11:31:05 +0200

    add SaveFileDialog

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,9 @@ namespace VaR
 
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> nyereségekRendezve;
+
+        public object Encodeing { get; private set; }
 
         public Form1()
         {
@@ -92,7 +96,7 @@ namespace VaR
                 Console.WriteLine(i + " " + ny);
             }
 
-            var nyereségekRendezve = (from x in Nyereségek
+             nyereségekRendezve = (from x in Nyereségek
                                       orderby x
                                       select x)
                                         .ToList();
@@ -122,6 +126,29 @@ namespace VaR
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+
+            if (sfd.ShowDialog()!=DialogResult.OK) return;
+
+
+
+
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.WriteLine("nyereség;rendezve");
+                for (int i = 0; i < nyereségekRendezve.Count(); i++)
+                {
+
+
+                    sw.WriteLine(string.Format("{0};{1}", i, nyereségekRendezve[i]));
+                }
+            }
         }
     }
 }
