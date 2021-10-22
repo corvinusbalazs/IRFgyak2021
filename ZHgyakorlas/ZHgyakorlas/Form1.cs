41e0830d7cc63e711af251c4990c4c0c94723fad
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-22 13:14:46 +0200

    add and set property for ramen.csv

--- ZHgyakorlas/ZHgyakorlas/Form1.cs
+++ ZHgyakorlas/ZHgyakorlas/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZHgyakorlas.Folder;
 
 namespace ZHgyakorlas
 {
     public partial class Form1 : Form
     {
+        List<Ramen> ramens = new List<Ramen>();
         public Form1()
         {
             InitializeComponent();
