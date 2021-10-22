57d0b145bea314aec30b1a7976f8c423d7b60b54
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-10-22 20:57:00 +0200

    add icon

--- probaweek/probaweek/Form1.Designer.cs
+++ probaweek/probaweek/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace probaweek
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
@@ -44,10 +45,13 @@ namespace probaweek
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Red;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
