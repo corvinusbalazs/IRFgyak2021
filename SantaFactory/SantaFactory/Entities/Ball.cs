e8a75587aa89304934a87dbd28fae8159af03407
Balazs Kiss <balazs.kiss5@stud.uni-corvinus.hu>
2021-11-12 09:48:09 +0100

    add new class Ball

---
+++ SantaFactory/SantaFactory/Entities/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SantaFactory.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            //Height = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+            var BallColor = Color.Blue;
+
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
